a4af48710a32241764ebe47fb85d00989f22bc2f
CMLCTRLEMEA - Sylvain Aillaud <sylvain.aillaud@carrier.com>
2019-11-26 15:03:07 +0100

    Initial Commit

---
+++ SA_Shop/SA_Shop/SA_Shop.cpp
@@ -0,0 +1,13 @@
+// SA_Shop.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
+// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
+
